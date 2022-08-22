ec892b7cb408fea7b753a5c15de087deec2868ab
AkhilJoga <111341918+AkhilJoga@users.noreply.github.com>
2022-08-22 09:39:49 +0530

    adding

--- src/main/java/com/jwt/demo/controller/HomeController.java
+++ src/main/java/com/jwt/demo/controller/HomeController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -43,5 +44,10 @@ public class HomeController {
 		}catch(BadCredentialsException e) {
 			throw new Exception("Invalid_Credentials",e);
 		}
+		
+		final UserDetails userDetails = userService.loadUserByUsername(jwtRequest.getUsername());
+		final String token = jwtUtility.getnerateToken(userDetails);
+		
+		return new JWTResponse(token);
 	}
 }
